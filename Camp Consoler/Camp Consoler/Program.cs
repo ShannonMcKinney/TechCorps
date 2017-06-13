d10357ba9b7dbdcdd232aa35a55c7cbefa56f19b
Shannon McKinney <20.shannon.mckinney@westg.org>
2017-06-13 11:32:30 -0400

    Changes

---
+++ Camp Consoler/Camp Consoler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camp_Consoler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What time is it?");
+            int time = Console.ReadLine();
+            if ((time >= 12) && (time <= 16))
+            {
+                Console.WriteLine("What is the air temp?");
+                int tair = Console.ReadLine();
+                if (tair >= 80)
+                { Console.WriteLine("What is the water temp?");
+                    int twater = Console.ReadLine();
+                    if (twater >= 70)
+                        Console.WriteLine()
+            }
+            else if ((time >= 12) && (time <= 16))
+            {
+                Console.WriteLine("What is the water temperature?");
+            }
+                
+
+                 
+        }
+    }
+}
