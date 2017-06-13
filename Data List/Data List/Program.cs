974a4d2151df3264af48e340aced28d356ba1b15
Shannon McKinney <20.shannon.mckinney@westg.org>
2017-06-13 14:12:38 -0400

    List

---
+++ Data List/Data List/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> students = new List<string>();
+            students.Add("John");
+            students.Add("Thomas");
+            students.Add("Joseph");
+            students.Add("Jacob");
+            students.Add("Parker");
+
+
+        }
+    }
+}
