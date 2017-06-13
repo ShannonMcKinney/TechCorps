c9d353eaeb06359f241c47ace547f5cfacd81340
Shannon McKinney <20.shannon.mckinney@westg.org>
2017-06-13 13:49:17 -0400

    Arrays/while/for loops

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentInClass = new string[5];
+            studentInClass[0] = "Colin";
+            studentInClass[1] = "Anthony";
+            studentInClass[2] = "Jeffery";
+            studentInClass[3] = "Shannon";
+            studentInClass[4] = "Ani";
+
+            //int i = 0;
+            //while (i < studentInClass.Length)
+            //{
+            //    Console.WriteLine(studentInClass[i]);
+            //    i++;
+            //}
+            for (int i = 0; i < studentInClass.Length; i++)
+            {
+                Console.WriteLine(studentInClass[i]);
+            }
+        }
+    }
+}
