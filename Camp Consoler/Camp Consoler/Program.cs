5d9595d5552301be613ff9307d2eb81acf9ecc09
Shannon McKinney <20.shannon.mckinney@westg.org>
2017-06-13 12:56:40 -0400

    ands and ifs and if elses

--- Camp Consoler/Camp Consoler/Program.cs
+++ Camp Consoler/Camp Consoler/Program.cs
@@ -10,25 +10,36 @@ namespace Camp_Consoler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What time is it?");
-            int time = Console.ReadLine();
-            if ((time >= 12) && (time <= 16))
-            {
-                Console.WriteLine("What is the air temp?");
-                int tair = Console.ReadLine();
-                if (tair >= 80)
-                { Console.WriteLine("What is the water temp?");
-                    int twater = Console.ReadLine();
-                    if (twater >= 70)
-                        Console.WriteLine()
-            }
-            else if ((time >= 12) && (time <= 16))
-            {
-                Console.WriteLine("What is the water temperature?");
-            }
-                
+            double twater;
+            double tair;
+            int prain;
+            int time;
 
-                 
+            Console.WriteLine("Please enter water temp. ");
+            twater = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter air temp. ");
+            tair = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter time");
+            time = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter chance of rain ");
+            prain = Convert.ToInt32(Console.ReadLine());
+
+            if ((time >= 12) && (time <= 16) && twater >= 70 && tair > 80 && prain < 30)
+            {
+                Console.WriteLine("Take the kids swimming");
+            }
+            else if (tair > 60 && prain < 50 && time >= 10 && time <= 18)
+            {
+                Console.WriteLine("Go play soccer");
+            }
+            else if (time >= 18 || prain <= 60)
+            {
+                Console.WriteLine("play a board game");
+            }
+            else if (time > 20)
+            {
+                Console.WriteLine("watch a movie");
+            }
         }
     }
 }
