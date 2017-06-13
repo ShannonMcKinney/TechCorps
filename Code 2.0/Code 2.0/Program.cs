fcfbcf158a65e16d29cc1240239bc680d526ad4e
ShannonMcKinney <20.shannon.mckinney@westg.org>
2017-06-13 13:03:46 -0400

    Add files via upload

---
+++ Code 2.0/Code 2.0/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello and welcome to the world of MemeÉMeme. My name is JEFF...");
+            Console.Write("Ummmm... I mean  Professor Jeff. Here, Memes roam freely and people called Meme Trainors catch   and battle each other with their Memes.");
+            Console.Write(" So what do you say? Want to start your   MemeÉMeme adventure?");
+            Console.Write(" Are you a boy or a girl?  ");
+            string answer = Console.ReadLine();
+            if (answer == "girl")
+            {
+                Console.Write("And what's your name?");
+                
+            }
+            else if (answer == "boy")
+            {
+
+            }
+            else
+            {
+                Console.Write("What?! Tell me, are you a boy or a girl?");
+            }
+            string answers = Console.ReadLine();
+
+        }
+    }
+}
