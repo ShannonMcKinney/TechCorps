df3c2669bd7c5444f5420172ae7a322ccda74b8f
Shannon McKinney <20.shannon.mckinney@westg.org>
2017-06-14 10:19:14 -0400

    find the three bugs

--- ConsoleApplication3/ConsoleApplication3/Program.cs
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -8,8 +8,37 @@ namespace ConsoleApplication3
 {
     class Program
     {
+        static string createNameString(string firstName, string lastName)
+        {
+            string fullname = firstName + " " + lastName;
+            return fullname;
+        }
         static void Main(string[] args)
         {
+            int score = 
+            string grade = "A";
+            if (score > 79 && score < 90)
+            {
+                grade = "B";
+            }
+            else if (score > 69 && score < 80)
+            {
+                grade = "C";
+            }
+            else if (score > 59 && score < 70)
+            {
+                grade = "D";
+            }
+            else
+            {
+                grade = "F";
+            }
+            Console.WriteLine(firstName + " " + lastName + ": " + grade);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createNameString("Iona", "Ford"));
+            giveStudentGrade("Tim", "Burr", 80);
         }
     }
 }
